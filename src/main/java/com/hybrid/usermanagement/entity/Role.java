7d55da1c23a978087098781e856ada6c0c313f4f
thuatnt <thuatnt@hybrid-technologies.vn>
2021-04-12 11:02:15 +0700

    init

---
+++ src/main/java/com/hybrid/usermanagement/entity/Role.java
@@ -0,0 +1,19 @@
+package com.hybrid.usermanagement.entity;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "role")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private List<User> users;
+
+}
---
+++ src/main/java/com/hybrid/usermanagement/entity/User.java
@@ -0,0 +1,40 @@
+package com.hybrid.usermanagement.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+@Table(name = "user")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "tel")
+    private String tel;
+
+    @Column(name = "address")
+    private String address;
+
+    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private List<Role> roles;
+}
